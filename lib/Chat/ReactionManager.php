wiegell/spreed

Api changes to react to a message: reactions add
Signed-off-by: Vitor Mattos <vitor@php.rio>

lib/Chat/ReactionManager.php
@@ -1,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2021 Vitor Mattos <vitor@php.rio>
+ *
+ * @author Vitor Mattos <vitor@php.rio>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Talk\Chat;
+
+use OCA\Talk\Participant;
+use OCA\Talk\Room;
+use OCP\Comments\IComment;
+use OCP\Comments\ICommentsManager;
+
+class ReactionManager {
+	/** @var ICommentsManager|CommentsManager */
+	private $commentsManager;
+
+	public function __construct(CommentsManager $commentsManager) {
+		$this->commentsManager = $commentsManager;
+	}
+
+	public function addReactionMessage(Room $chat, Participant $participant, int $messageId, string $emoji): IComment {
+		$comment = $this->commentsManager->create(
+			$participant->getAttendee()->getActorType(),
+			$participant->getAttendee()->getActorId(),
+			'chat',
+			(string) $chat->getId()
+		);
+		$comment->setParentId((string) $messageId);
+		$comment->setMessage($emoji);
+		$comment->setVerb('reaction');
+		$this->commentsManager->save($comment);
+		return $comment;
+	}
+}

lib/Controller/ReactionController.php
@@ -1,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2021 Vitor Mattos <vitor@php.rio>
+ *
+ * @author Vitor Mattos <vitor@php.rio>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Talk\Controller;
+
+use OCA\Talk\Chat\CommentsManager;
+use OCA\Talk\Chat\ReactionManager;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\Comments\NotFoundException;
+use OCP\IRequest;
+
+class ReactionController extends AEnvironmentAwareController {
+	/** @var CommentsManager */
+	private $commentsManager;
+	/** @var ReactionManager */
+	private $reactionManager;
+
+	public function __construct(string $appName,
+								IRequest $request,
+								CommentsManager $commentsManager,
+								ReactionManager $reactionManager) {
+		parent::__construct($appName, $request);
+
+		$this->commentsManager = $commentsManager;
+		$this->reactionManager = $reactionManager;
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @RequireParticipant
+	 * @RequireReadWriteConversation
+	 * @RequireModeratorOrNoLobby
+	 *
+	 * @param int $messageId for reaction
+	 * @param string $emoji the reaction emoji
+	 * @return DataResponse
+	 */
+	public function react(int $messageId, string $emoji): DataResponse {
+		$participant = $this->getParticipant();
+		try {
+			// Verify if messageId is of room
+			$this->commentsManager->getComment($this->getRoom(), (string) $messageId);
+		} catch (NotFoundException $e) {
+			return new DataResponse([], Http::STATUS_NOT_FOUND);
+		}
+
+		try {
+			// Verify already reacted whith the same reaction
+			$this->commentsManager->getReactionComment(
+				$messageId,
+				$participant->getAttendee()->getActorType(),
+				$participant->getAttendee()->getActorId(),
+				$emoji
+			);
+			return new DataResponse([], Http::STATUS_CONFLICT);
+		} catch (NotFoundException $e) {
+		}
+
+		try {
+			$this->reactionManager->addReactionMessage($this->getRoom(), $participant, $messageId, $emoji);
+		} catch (\Exception $e) {
+			return new DataResponse([], Http::STATUS_BAD_REQUEST);
+		}
+
+		return new DataResponse([], Http::STATUS_CREATED);
+	}
+}

devopsleague/nextcloud-spreed

lib/Chat/ReactionManager.php
@@ -1,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2021 Vitor Mattos <vitor@php.rio>
+ *
+ * @author Vitor Mattos <vitor@php.rio>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Talk\Chat;
+
+use OCA\Talk\Participant;
+use OCA\Talk\Room;
+use OCP\Comments\IComment;
+use OCP\Comments\ICommentsManager;
+
+class ReactionManager {
+	/** @var ICommentsManager|CommentsManager */
+	private $commentsManager;
+
+	public function __construct(CommentsManager $commentsManager) {
+		$this->commentsManager = $commentsManager;
+	}
+
+	public function addReactionMessage(Room $chat, Participant $participant, int $messageId, string $reaction): IComment {
+		$comment = $this->commentsManager->create(
+			$participant->getAttendee()->getActorType(),
+			$participant->getAttendee()->getActorId(),
+			'chat',
+			(string) $chat->getId()
+		);
+		$comment->setParentId((string) $messageId);
+		$comment->setMessage($reaction);
+		$comment->setVerb('reaction');
+		$this->commentsManager->save($comment);
+		return $comment;
+	}
+}

lib/Controller/ReactionController.php
@@ -1,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2021 Vitor Mattos <vitor@php.rio>
+ *
+ * @author Vitor Mattos <vitor@php.rio>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Talk\Controller;
+
+use OCA\Talk\Chat\CommentsManager;
+use OCA\Talk\Chat\ReactionManager;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\Comments\NotFoundException;
+use OCP\IRequest;
+
+class ReactionController extends AEnvironmentAwareController {
+	/** @var CommentsManager */
+	private $commentsManager;
+	/** @var ReactionManager */
+	private $reactionManager;
+
+	public function __construct(string $appName,
+								IRequest $request,
+								CommentsManager $commentsManager,
+								ReactionManager $reactionManager) {
+		parent::__construct($appName, $request);
+
+		$this->commentsManager = $commentsManager;
+		$this->reactionManager = $reactionManager;
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @RequireParticipant
+	 * @RequireReadWriteConversation
+	 * @RequireModeratorOrNoLobby
+	 *
+	 * @param int $messageId for reaction
+	 * @param string $reaction the reaction emoji
+	 * @return DataResponse
+	 */
+	public function react(int $messageId, string $reaction): DataResponse {
+		$participant = $this->getParticipant();
+		try {
+			// Verify if messageId is of room
+			$this->commentsManager->getComment($this->getRoom(), (string) $messageId);
+		} catch (NotFoundException $e) {
+			return new DataResponse([], Http::STATUS_NOT_FOUND);
+		}
+
+		try {
+			// Verify already reacted whith the same reaction
+			$this->commentsManager->getReactionComment(
+				$messageId,
+				$participant->getAttendee()->getActorType(),
+				$participant->getAttendee()->getActorId(),
+				$reaction
+			);
+			return new DataResponse([], Http::STATUS_CONFLICT);
+		} catch (NotFoundException $e) {
+		}
+
+		try {
+			$this->reactionManager->addReactionMessage($this->getRoom(), $participant, $messageId, $reaction);
+		} catch (\Exception $e) {
+			return new DataResponse([], Http::STATUS_BAD_REQUEST);
+		}
+
+		return new DataResponse([], Http::STATUS_CREATED);
+	}
+}